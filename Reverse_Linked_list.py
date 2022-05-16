cc46ac5b73033e50fe27115b76f5908aba8ca1ca
chanwoo yang <moomsss@naver.com>
2022-05-16 16:11:44 +0900

    역순 연결리스트 알고리즘 공부

---
+++ Reverse_Linked_list.py
@@ -0,0 +1,17 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        node, prev = head, None
+
+        while node:
+            next, node.next = node.next, prev
+            prev, node = node, next
+
+        return prev
+j = Solution()
+print(j.reverseList( [1, 2, 3, 4, 5]))

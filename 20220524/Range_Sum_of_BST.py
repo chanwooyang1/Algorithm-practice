1dbd4ea2c6f09a2c5041e61872e8cd9ec947a15c
chanwoo yang <moomsss@naver.com>
2022-05-24 23:46:48 +0900

    20220524 이진트리 알고리즘 공부

---
+++ 20220524/Range_Sum_of_BST.py
@@ -0,0 +1,20 @@
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        sum = 0
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                if low <= node.val <= high:
+                    sum += node.val
+                if low < node.val:
+                    stack.append(node.left)
+                if node.val < high:
+                    stack.append(node.right)
+        return sum
